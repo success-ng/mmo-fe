39ed517988f1650cab56890c33a1e7119e848415
Your Name <your.email@example.com>
2025-03-07 11:17:23 +0700

    feat: add ecosystem configuration for production environment setup

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "nitro-app",
+      script: ".output/server/index.mjs",
+      interpreter: "node",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
