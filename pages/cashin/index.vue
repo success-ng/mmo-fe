343e87331a4a6f16f35193159fba0034398aec95
Your Name <your.email@example.com>
2025-03-28 00:10:03 +0700

    fix: correct cash-in amount options by adjusting maximum value

--- pages/cashin/index.vue
+++ pages/cashin/index.vue
@@ -7,7 +7,7 @@
    const userService = useApiUserService();
    const config = useRuntimeConfig();
    const cashInAmt = [
-      50000, 100000, 200000, 500000, 1000000, 2000000, 5000000, 70000000,
+      50000, 100000, 200000, 500000, 1000000, 2000000, 5000000, 7000000,
    ];
 
    const loading = ref<boolean>(false);
