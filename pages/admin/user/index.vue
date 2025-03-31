abdb5b740740b0ea105c27891bbe77a96db605a4
Your Name <your.email@example.com>
2025-03-31 15:11:49 +0700

    fix: reverse user list order in admin panel

--- pages/admin/user/index.vue
+++ pages/admin/user/index.vue
@@ -56,7 +56,7 @@
       <MaterialTable
          title="Danh sách người dùng"
          :is-loading="isLoading"
-         :data="users"
+         :data="users.reverse()"
          :key-words="keyWords"
          :columns="columns"
          :fetch="fetch">
